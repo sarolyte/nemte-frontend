01e15739fca1a802ee7a749ec4f880d8552c9354
sarolyte <meda.sarolyte@gmail.com>
2025-10-09 10:20:43 +0300

    add breakpoints, media mixins, and useBreakpoints hook

---
+++ src/hooks/useBreakpoints.js
@@ -0,0 +1,9 @@
+import { useMediaQuery } from 'react-responsive'
+
+export const useBreakpoints = () => {
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+    const isDesktop = useMediaQuery({ minWidth: 992 });
+
+    return { isMobile, isTablet, isDesktop };
+}
---
+++ src/styles/mixins.js
@@ -0,0 +1,8 @@
+import { breakpoints } from "./breakpoints.js"
+
+export const media = {
+    sm: `(min-width: ${breakpoints.sm})`,
+    md: `(min-width: ${breakpoints.md})`,
+    lg: `(min-width: ${breakpoints.lg})`,
+    xl: `(min-width: ${breakpoints.xl})`,
+}
